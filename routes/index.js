0361c604c88884e964f738a042fb24e15d9389f4
MasakiAyano <masakiayano@MasakiAyano-no-MacBook-Pro.local>
2016-10-25 15:10:54 +0900

    create post form

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res){
+  //console.log(req.query); // for logging
+  var HTMLtext = "";
+  if (req.body.htmltext) {
+    HTMLtext = req.body.htmltext[0];
+  }
+  console.log("HTMLText:" + HTMLtext);
+
+
+  res.render('index', { title: 'Express' });
+});
+
 module.exports = router;
